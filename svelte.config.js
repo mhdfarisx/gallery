ed5bc7fd389310394da09929797942bce75c9869
mhdfarisx <mhdfaris606@gmail.com>
2023-03-13 11:36:40 +0800

    First commit

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,21 @@
-import adapter from '@sveltejs/adapter-auto';
-
+import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+ // Consult https://github.com/sveltejs/svelte-preprocess
+ // for more information about preprocessors
+ preprocess: preprocess(),
+
+ kit: {
+ // hydrate the <div id="svelte"> element in src/app.html
+  target: '#svelte',
+  adapter: adapter({
+    pages: 'build',
+    assets: 'build',
+    fallback: null
+  })
+ }
 };
 
 export default config;
